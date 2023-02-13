bc8bce3ee4ccb25f8be2bc2d2d059e1b3856b530
Deiber Duban Olivares Olivares <dolivare5@cuc.edu.co>
2023-02-12 23:40:15 -0500

    Actualización del archivo user_role.guard.ts

--- src/modules/usuario/guards/user-role.guard.ts
+++ src/modules/usuario/guards/user-role.guard.ts
@@ -10,15 +10,22 @@ import { RolesPermitidos } from '../interfaces/roles-permitidos';
 export class UserRoleGuard implements CanActivate {
     private readonly exceptionService: ExceptionsService = new ExceptionsService();
     constructor(
-        private readonly servicioDeBaseDeDatos: IBaseDeDatosAbstract,
-        private readonly reflector: Reflector
+      private readonly servicioDeBaseDeDatos: IBaseDeDatosAbstract,
+      private readonly reflector: Reflector
     ) {}
     
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const asyncFunction = async () => {
             let roles_permitidos: string[] = this.reflector.get(META_ROLES, context.getHandler());
             context.switchToHttp().getRequest().url;
-            const token = context.switchToHttp().getRequest().rawHeaders[1].split(' ')[1];
+            let token =
+              context.switchToHttp().getRequest().rawHeaders[1].split(' ')[1] ||
+              context.switchToHttp().getRequest().rawHeaders[5].split(' ')[1];
+            if (token === undefined) {
+                this.exceptionService.forbiddenException({message: 'Se requiere un token de autenticación'});
+                return false;
+            }
+            
             const tipoDeUsuario = (await this.servicioDeBaseDeDatos.usuario.executeQuery(`
                 SELECT vp.id, LOWER(vp.nombre) AS rol FROM usuario u
                 JOIN valor_parametro vp ON u.id_rol = vp.id
@@ -29,11 +36,13 @@ export class UserRoleGuard implements CanActivate {
             
             if(!roles_permitidos || roles_permitidos.length === 0) roles_permitidos = [RolesPermitidos.usuario_conencional];
             if(roles_permitidos.includes(tipoDeUsuario.rol)) return true;
-
+            
             this.exceptionService.forbiddenException({message: 'No tienes permisos para acceder a este recurso'});
-            return false;        
+            return false;
             
         };
         return asyncFunction();
+        
+        
     }
 }
