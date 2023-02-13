67ea024951b0112a478e9dd036f996ce203995c1
Deiber Duban Olivares Olivares <dolivare5@cuc.edu.co>
2023-02-12 23:41:33 -0500

    Actualización del archivo user_role.guard.ts

--- src/modules/usuario/guards/user-role.guard.ts
+++ src/modules/usuario/guards/user-role.guard.ts
@@ -5,34 +5,27 @@ import {ExceptionsService} from "../../../config/exceptions/exceptions.service";
 import {Reflector} from "@nestjs/core";
 import {META_ROLES} from "../decorators/role-protected.decorator";
 import { RolesPermitidos } from '../interfaces/roles-permitidos';
-import { Usuario } from "../../../framework/database/mysql/entities";
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
     private readonly exceptionService: ExceptionsService = new ExceptionsService();
     constructor(
-        private readonly servicioDeBaseDeDatos: IBaseDeDatosAbstract,
-        private readonly reflector: Reflector
+      private readonly servicioDeBaseDeDatos: IBaseDeDatosAbstract,
+      private readonly reflector: Reflector
     ) {}
     
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const asyncFunction = async () => {
             let roles_permitidos: string[] = this.reflector.get(META_ROLES, context.getHandler());
-            console.log(context.switchToHttp().getRequest().rawHeaders);
-            console.log(context.switchToHttp().getRequest().rawHeaders[5]);
-            const pathUrl = context.switchToHttp().getRequest().url;
-            const user = context.switchToHttp().getRequest().user as Usuario;
             context.switchToHttp().getRequest().url;
-            let token = context.switchToHttp().getRequest().rawHeaders[1].split(' ')[1];
+            let token =
+              context.switchToHttp().getRequest().rawHeaders[1].split(' ')[1] ||
+              context.switchToHttp().getRequest().rawHeaders[5].split(' ')[1];
             if (token === undefined) {
-                token = context.switchToHttp().getRequest().rawHeaders[5].split(' ')[1];
-                if (token === undefined) {
-                    this.exceptionService.forbiddenException({message: 'Se requiere un token de autenticación'});
-                    return false;
-                }
+                this.exceptionService.forbiddenException({message: 'Se requiere un token de autenticación'});
+                return false;
             }
             
-            console.log('token', token);
             const tipoDeUsuario = (await this.servicioDeBaseDeDatos.usuario.executeQuery(`
                 SELECT vp.id, LOWER(vp.nombre) AS rol FROM usuario u
                 JOIN valor_parametro vp ON u.id_rol = vp.id
@@ -40,11 +33,10 @@ export class UserRoleGuard implements CanActivate {
                 WHERE ru.token = '${token}' AND ru.fecha_salida IS NULL
                 
             `))[0] ;
-            console.log('tipoDeUsuario', tipoDeUsuario);
             
             if(!roles_permitidos || roles_permitidos.length === 0) roles_permitidos = [RolesPermitidos.usuario_conencional];
             if(roles_permitidos.includes(tipoDeUsuario.rol)) return true;
-
+            
             this.exceptionService.forbiddenException({message: 'No tienes permisos para acceder a este recurso'});
             return false;
             
